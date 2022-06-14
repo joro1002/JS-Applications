d3a0c3f1dab7f88a52691f70c39892697e1f2c18
joro1002 <joro1002@abv.bg>
2022-06-14 18:43:14 +0300

    Blog

--- AsynchronousProgramming/06.Blog/app.js
+++ AsynchronousProgramming/06.Blog/app.js
@@ -1,5 +1,69 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('btnLoadPosts').addEventListener('click', getAllPosts);
+    document.getElementById('btnViewPost').addEventListener('click', displayPost);
 }
 
 attachEvents();
+
+async function getAllPosts(){
+    const url = "http://localhost:3030/jsonstore/blog/posts";
+
+    const res = await fetch(url);
+
+    const data = await res.json();
+
+    const selectElement = document.getElementById('posts');
+
+    Object.values(data).forEach(p => {
+        const optionEl = document.createElement('option');
+        optionEl.textContent = p.title;
+        optionEl.value = p.id;
+
+        selectElement.appendChild(optionEl);
+    })
+}
+
+async function displayPost(){
+    const titleElement = document.getElementById('post-title');
+    const bodyElement = document.getElementById('post-body');
+    const ulElement = document.getElementById('post-comments');
+
+    bodyElement.textContent = '';
+    ulElement.replaceChildren();
+
+    const selectedId = document.getElementById('posts').value;
+
+   const[post, comments] =  await Promise.all([
+        getPostById(selectedId),
+        getCommentsByPostId(selectedId)
+    ]);
+
+    titleElement.textContent = post.title;
+    bodyElement.textContent = post.body;
+
+    comments.forEach(c => {
+        const liElement = document.createElement('li');
+        liElement.textContent = c.text;
+        ulElement.appendChild(liElement);
+    })
+}
+
+async function getPostById(postId){
+    const url = 'http://localhost:3030/jsonstore/blog/posts/' + postId;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return data;
+}
+
+async function getCommentsByPostId(postId){
+    const url = 'http://localhost:3030/jsonstore/blog/comments';
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    const comments = Object.values(data).filter(c => c.postId = postId);
+
+    return comments;
+}
