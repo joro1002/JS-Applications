ed68705c73e0ecd31bb65e410e09966a56ffa31d
joro1002 <joro1002@abv.bg>
2022-06-20 15:25:50 +0300

    BookLibrary

--- DataAndAuthentication/04.Book-library/app.js
+++ DataAndAuthentication/04.Book-library/app.js
@@ -1,1 +1,56 @@
 console.log('My requests...')
+
+async function request(url, options){
+    if(options && options.body != undefined){
+        Object.assign(options, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+        });
+    }
+
+    const res = await fetch(url, options);
+
+    if(res.ok != true){
+        const error = await res.json();
+        alert(error.message);
+
+        throw new Error(error.message);
+    }
+
+    const data = await res.json();
+    return data;
+}
+
+async function loadBooks(){
+    const books = await request('http://localhost:3030/jsonstore/collections/books');
+
+    return books;
+}
+
+async function createBook(book){
+    const result = await request('http://localhost:3030/jsonstore/collections/books', {
+        method: 'post',
+        body: JSON.stringify(book)
+    });
+
+    return result;
+}
+
+async function updateBook(id, book){
+    const result = await request('http://localhost:3030/jsonstore/collections/books/' + id, {
+        method: 'put',
+        body: JSON.stringify(book)
+    });
+
+    return result;
+}
+
+async function deleteBook(id){
+    const result = await request('http://localhost:3030/jsonstore/collections/books/' + id, {
+        method: 'delete'
+    });
+
+    return result;
+}
